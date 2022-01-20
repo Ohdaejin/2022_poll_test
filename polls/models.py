acfd8b54734e33a4df50eb7a6704da64889b567a
Ohdaejin <xla3095@gmail.com>
2022-01-20 13:01:52 +0900

    투표 기능 구현

--- polls/models.py
+++ polls/models.py
@@ -7,17 +7,9 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')  # 데이터 타입 날짜
 
 
-def __str__(self):
-    return self.question_text
-
-
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)  # 칼럼은 외래키로 Question과 연결됨
     choice_text = models.CharField(max_length=200)  # 문자열
     votes = models.IntegerField(default=0)  # 정수
-
-
-def __str__(self):
-    return self.choice_text
 
 # 테이블 두개 만드는 것 부모테이블 자식테이블
